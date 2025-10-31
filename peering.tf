153a49e594d872aaced9d030c868156dd277a60c
sainath <kollavenkatasai999@gmail.com>
2025-10-31 19:11:26 -0400

    minor change

--- peering.tf
+++ peering.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc_peering_connection" "peering" {
     count = var.is_peering_required ? 1 : 0
-    vpc_id        = aws_vpc.expense.id # requestor
-    peer_vpc_id   = var.acceptor_vpc_id == {} ? data.aws_vpc.default.id : var.acceptor_vpc_id # acceptor
-    auto_accept   = var.acceptor_vpc_id == {} ? true : false 
+    vpc_id        = aws_vpc.main.id # requestor
+    peer_vpc_id   = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id # acceptor
+    auto_accept   = var.acceptor_vpc_id == "" ? true : false 
     tags = merge (
         var.common_tags,
         var.vpc_peering_tags,
